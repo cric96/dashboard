db151fcdde14b5e5f37d25ebeba2ccf9ed4d9538
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-07-07 18:23:27 +0200

    chore: create map component

---
+++ src/components/map/MarkerComponent.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MarkerComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/structure/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <Sidebar v-model:visible="sideVisible" position="right" class="p-sidebar-md" @hide="hide">
+    <slot name="header"></slot>
+    <Divider/>
+    <slot name="content"></slot>
+  </Sidebar>
+</template>
+
+<script>
+import Sidebar from 'primevue/sidebar';
+import Divider from 'primevue/divider';
+
+export default {
+  name: "CPSidebar",
+  components:{
+    Sidebar,
+    Divider,
+  },
+  data(){
+    return{
+      sideVisible:this.sideVisibility
+    }
+  },
+  props: [
+      'sideVisibility',
+  ],
+  watch: {
+    sideVisibility(n){
+      this.sideVisible = n;
+    },
+  },
+  methods:{
+    hide(){
+      console.log(this.sideVisible + "    " + this.sideVisibility)
+      this.$emit('closed');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
