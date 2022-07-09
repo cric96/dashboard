846f84610a27ea13a90f7780a005b52ec21ce72d
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-07-09 09:56:37 +0200

    chore: package refactoring

---
+++ src/components/map/sidebar/CollectionPointSidebar.vue
@@ -0,0 +1,62 @@
+<template>
+  <Sidebar>
+    <template #header>
+      <h1> Collection Point {{ collectionPointId }} </h1>
+      <p />
+    </template>
+
+    <template #content>
+      <div class="flex flex-column justify-content-evenly">
+        Prova
+      </div>
+    </template>
+  </Sidebar>
+</template>
+
+<script>
+import Sidebar from '@/components/map/sidebar/Sidebar';
+
+export default {
+	name: 'CollectionPointSidebar',
+	components:{
+		Sidebar,
+	},
+	props:{
+		'collectionPointId': {
+			type: String,
+			default:'000',
+		},
+	},
+	data (){
+		return {
+			collectionPoint:null,
+		};
+	},
+	watch:{
+		collectionPointId (n){
+			console.log(n);
+		}
+	},
+	mounted () {
+		console.log('Collection Point showed' + this.collectionPointId);
+	},
+	methods:{
+		// getTruckById (){
+		// 	axios.get(process.env.VUE_APP_TRUCK_MICROSERVICE+'/trucks/'+this.collectionPointId, {
+		// 		headers: {
+		// 			'Access-Control-Allow-Origin': '*',
+		// 			'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+		// 			'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+		// 		}
+		// 	})
+		// 		.then(res => {
+		// 			this.truck = res.data;
+		// 		});
+		// }
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/map/sidebar/DumpsterCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <Card>
+    <template #title>
+      Dumpster id: {{ dumpster }}
+    </template>
+    <template #content>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+      quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+    </template>
+  </Card>
+</template>
+
+<script>
+import Card from 'primevue/card';
+export default {
+	name: 'DCard',
+	components:{
+		Card,
+	},
+	props:[
+		'dumpster'
+	]
+};
+</script>
+
+<style scoped>
+
+</style>
