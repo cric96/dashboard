eb3318a4718e4fd540fcf87e0bb9937e9450a406
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-07-09 13:26:28 +0200

    chore: add best mock data

--- src/components/map/sidebar/CollectionPointSidebar.vue
+++ src/components/map/sidebar/CollectionPointSidebar.vue
@@ -1,17 +1,19 @@
 <template>
   <Sidebar @item-updated="updateCollectionPointId">
     <template #header>
-      <h1> Collection Point {{ collectionPointId }} </h1>
-      <p />
+      <h1> {{ collectionPointId }} </h1>
+      <p v-if="collectionPoint !== null">
+        {{ collectionPoint.position }}
+      </p>
     </template>
 
     <template
-      v-if="collectionPoint!==null"
+      v-if="dumpsters!==null"
       #content
     >
       <div class="flex flex-column justify-content-evenly">
         <DumpsterCard
-          v-for="d in collectionPoint.dumpsters"
+          v-for="d in dumpsters"
           :key="d"
           class="flex align-items-center justify-content-center m-2"
           :dumpster="d"
@@ -24,6 +26,7 @@
 <script>
 import Sidebar from '@/components/map/sidebar/Sidebar';
 import DumpsterCard from '@/components/map/sidebar/DumpsterCard';
+// import axios from 'axios';
 
 export default {
 	name: 'CollectionPointSidebar',
@@ -35,11 +38,13 @@ export default {
 		return {
 			collectionPointId:null,
 			collectionPoint:null,
+			dumpsters:[],
 		};
 	},
 	methods:{
-		// getDumpsterById (){
-		// 	axios.get(process.env.VUE_APP_TRUCK_MICROSERVICE+'/trucks/'+this.collectionPointId, {
+		// getCollectionPointById (){
+		// 	this.dumpsters = [];
+		// 	axios.get(process.env.VUE_APP_DUMPSTER_MICROSERVICE+'/collectionpoints/'+this.collectionPointId, {
 		// 		headers: {
 		// 			'Access-Control-Allow-Origin': '*',
 		// 			'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
@@ -47,30 +52,66 @@ export default {
 		// 		}
 		// 	})
 		// 		.then(res => {
-		// 			this.truck = res.data;
+		// 			console.log(res.data);
+		// 			this.collectionPoint = res.data;
 		// 		});
-		// }
+		// 	axios.get(process.env.VUE_APP_DUMPSTER_MICROSERVICE+'/collectionpoints/'+this.collectionPointId+'/dumpsters', {
+		// 		headers: {
+		// 			'Access-Control-Allow-Origin': '*',
+		// 			'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+		// 			'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+		// 		}
+		// 	})
+		// 		.then(res => {
+		// 			console.log(res);
+		// 			res.data.forEach(d => this.dumpsters.push(d));
+		// 		});
+		// },
 		getCollectionPointById (){
-			this.collectionPoint = {
-				id:this.collectionPointId,
-				dumpsters:[
-					{
-						id:'D-1',
-						status:'working',
+			this.dumpsters = [
+				{
+					id: 'Dumpster2',
+					dumpsterType: {
+						size: {
+							dimension: 'SMALL',
+							capacity: 175.0
+						},
+						typeOfOrdinaryWaste: {
+							wasteName: 'ORGANIC',
+							wasteColor: 'BROWN'
+						}
 					},
-					{
-						id:'D-2',
-						status:'working',
+					occupiedVolume: {
+						value: 0.0
 					},
-					{
-						id:'D-3',
-						status:'not-working',
-					}
-				]
-			};
+					open: false,
+					available: true,
+					working: true
+				},
+				{
+					id: 'Dumpster3',
+					dumpsterType: {
+						size: {
+							dimension: 'SMALL',
+							capacity: 175.0
+						},
+						typeOfOrdinaryWaste: {
+							wasteName: 'PAPER',
+							wasteColor: 'BLUE'
+						}
+					},
+					occupiedVolume: {
+						value: 175.0
+					},
+					open: false,
+					available: false,
+					working: true
+				}
+			];
 		},
 		updateCollectionPointId (id){
 			this.collectionPointId = id;
+			this.dumpsters = [];
 			this.getCollectionPointById();
 		},
 	},
