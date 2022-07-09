187b8935b0f8cbeecd9b8475dfd8c15c38d23832
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-07-09 13:26:28 +0200

    chore: add best mock data

--- src/components/map/CollectionPointMarkers.vue
+++ src/components/map/CollectionPointMarkers.vue
@@ -2,8 +2,8 @@
   <MarkerComponent
     v-for="cp in collectionPoints"
     :key="cp"
-    :lat="cp.lat"
-    :lng="cp.lng"
+    :lat="cp.position.latitude"
+    :lng="cp.position.longitude"
     :icon="cpIcon"
     @click="$emit('open-sidebar', cp.id)"
   />
@@ -13,8 +13,9 @@
 import MarkerComponent from '@/components/map/MarkerComponent';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-const randomLatitude = require('random-latitude');
-const randomLongitude = require('random-longitude');
+import axios from 'axios';
+import randomLatitude from 'random-latitude';
+import randomLongitude from 'random-longitude';
 export default {
 	name: 'CollectionPointMarkers',
 	components: {MarkerComponent},
@@ -23,21 +24,38 @@ export default {
 		return {
 			collectionPoints:[],
 			cpIcon: L.icon({
-				iconUrl: 'https://i.postimg.cc/WDjbgvLy/iconsBin.png',
+				iconUrl: 'https://i.postimg.cc/PfCmTmb2/collection-point.png',
 				iconSize:     [32, 32], // size of the icon
 			}),
 		};
 	},
 	mounted () {
-		for (let i = 0; i < 10; i++){
+		for (let i = 0; i < 4; i++){
 			let lat = randomLatitude({min:44.14, max:44.20});
 			let lng = randomLongitude({min:12.24, max:12.30});
 			this.collectionPoints.push({
-				id:'cp-'+i,
-				lat:lat,
-				lng: lng,
-				dumpsters:['1', '2', '3'],
+				id:'Cp-'+i,
+				position:{
+					latitude:lat,
+					longitude: lng,
+				}
 			});
+		}
+		//this.getCollectionPoints();
+	},
+	methods : {
+		getCollectionPoints (){
+			axios.get(process.env.VUE_APP_DUMPSTER_MICROSERVICE+'/collectionpoints', {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+					'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+				}
+			})
+				.then(res => {
+					console.log(res);
+					res.data.forEach(c => this.collectionPoints.push(c));
+				});
 		}
 	}
 };
