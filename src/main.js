fe838cd9f7728fdb07fc6d20e2196ac305462594
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2022-08-17 15:53:26 +0200

    chore: fix map dimension and add background

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import BadgeDirective from 'primevue/badgedirective';
 import 'primevue/resources/themes/lara-light-indigo/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import '/node_modules/primeflex/primeflex.css';
 import { createWebHistory, createRouter } from 'vue-router';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import MapComponent from '@/components/map/MapComponent';
 import MissionsList from '@/components/missions/MissionsList';
 import ComplaintsList from '@/components/complaints/ComplaintsList';
 import AccountComponent from '@/components/authentication/AccountComponent';
+import MissionDetails from '@/components/missions/MissionDetails';
+import BookingList from '@/components/bookings/BookingList';
+import BookingForm from '@/components/bookings/BookingForm';
 
 const routes = [
 	{
@@ -29,9 +36,24 @@ const routes = [
 		component: MissionsList,
 	},
 	{
+		path: '/dashboard/missions/:id',
+		name: 'Missions Details',
+		component: MissionDetails,
+	},
+	{
 		path: '/dashboard/complaints',
 		name: 'Complaints',
 		component: ComplaintsList,
+	},
+	{
+		path: '/dashboard/bookings',
+		name: 'Bookings',
+		component: BookingList,
+	},
+	{
+		path: '/dashboard/bookings/new',
+		name: 'Bookings Form',
+		component: BookingForm,
 	},
 	{
 		path: '/dashboard/account',
@@ -46,4 +68,12 @@ const router = createRouter({
 	routes,
 });
 
-createApp(App).use(PrimeVue).use(router).mount('#app');
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+const app = createApp(App);
+app.use(pinia);
+app.use(PrimeVue);
+app.use(ToastService);
+app.use(router);
+app.directive('badge', BadgeDirective);
+app.mount('#app');
